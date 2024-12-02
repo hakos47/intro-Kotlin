1620b273a86ddd0396dfd4cb2e8647df1446d5d3
Hakos <76844995+hakos47@users.noreply.github.com>
2024-12-02 11:38:16 +0100

    Update: input console

--- src/Numbers/Exercise-2.kt
+++ src/Numbers/Exercise-2.kt
@@ -1,9 +1,17 @@
 package Numbers
 
 object Exercise2 {
+    fun multiplication(v1:Int,v2:Int):Int{
+        return v1*v2
+    }
     fun execution() {
-        println("Exercise 2")
-        val result :Int = 2 * 2
-        println("result: " + result)
+        println("Exercise 2: Multiplication")
+        println("Value 1")
+        val v1: Int = readLine()!!.toInt()
+        println("Value 2")
+        val v2: Int = readLine()!!.toInt()
+        println ("Multiplicaiton $v1 * $v2 ")
+        var result:Int = multiplication(v1,v2)
+        println("Result: $result")
     }
 }
