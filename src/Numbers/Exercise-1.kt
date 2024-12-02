dd5a2ca69cfcad8247c4d47dd56895225c13663f
Hakos <76844995+hakos47@users.noreply.github.com>
2024-12-02 11:32:53 +0100

    Update: Console input

--- src/Numbers/Exercise-1.kt
+++ src/Numbers/Exercise-1.kt
@@ -1,9 +1,18 @@
 package Numbers
 
 object Exercise1 {
+    fun Suma(v1:Int,v2:Int):Int {
+        return v1+v2
+    }
     fun execution() {
         println("Exercise 1")
-        val result :Int = 2 + 2
-        println("result: " + result)
+        print("Valor 1")
+        val v1: Int = readLine()!!.toInt()
+        println("Valor 2: $v1")
+        val v2:Int = readLine()!!.toInt()
+        println("Suma $v1 + $v2")
+        var result = Suma(v1,v2)
+        print("Result: " + result)
+
     }
 }
